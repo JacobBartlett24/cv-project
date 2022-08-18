ef78b54ec4d7d596f4456c11c720f55338673f34
JacobBartlett24 <jacobart@ttu.edu>
2022-08-18 13:53:48 -0500

    Add work experience function

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,7 @@ const MainStyle = styled.div`
 const FormStyle = styled.div`
   display: flex;
   flex-direction: column;
+  
 `
 
 const Title = styled.div`
@@ -18,7 +19,7 @@ const Title = styled.div`
 `
 const ContainerStyle = styled.div`
   display: grid;
-  grid-template-rows: repeat(5,4rem);
+  grid-template-rows: repeat(6,4rem);
   height: 17rem;
   width: 50vw;
   padding: 1rem;
@@ -26,9 +27,22 @@ const ContainerStyle = styled.div`
   background: grey;
   `
 
-  const WorkExperience = styled.div`
-  
-  `
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  height: 6rem;
+  width: 50vw;  
+  background:grey;
+`
+
+const InputStyle = styled.div`
+  background: grey;
+  list-style: none;
+  width:50vw;
+  margin-top: 2rem;
+`
+
 class AppComponent extends Component{
   constructor(props){
     super(props)
@@ -44,10 +58,12 @@ class AppComponent extends Component{
     e.preventDefault();
     this.setState({
       child: {html: <div>
-                      <input type="text" placeholder="Name" id = 'names' style = {{height: 20}}/>
-                      <input type="text" placeholder="Name" id = 'name2' style = {{height: 20}}/>
-                      <input type="text" placeholder="Name" id = 'name3' style = {{height: 20}}/>
-                    </div>},
+        
+                        <input type="text" style = {{lineHeight: 2}}placeholder="Name" id = 'names' style = {{height: 20}}/>
+                        <input type="text" style = {{lineHeight: 2}}placeholder="Name" id = 'name2' style = {{height: 20}}/>
+                        <input type="text" style = {{lineHeight: 2}}placeholder="Name" id = 'name3' style = {{height: 20}}/>
+                    </div>
+      },
 
       children: this.state.children.concat(this.state.child),
     });
@@ -68,20 +84,24 @@ class AppComponent extends Component{
             <input type="number" placeholder="Number" id = 'number' style = {{height: 20}}/>
             <input type="email" placeholder="Email" id = 'email' style = {{height: 20}}/>
             <input type="text" placeholder="Desired Salary" id = 'salary' style = {{height: 20}}/> 
-            <ul>
-              {children.map((child) =>{
-                return <li>{child.html}</li>
-              })}
-            </ul>
+            <ListStyle>
+              <ul>
+                {children.map((child) =>{
+                  return <InputStyle>
+                    <li>{child.html}</li>
+                  </InputStyle>
+                })}
+              </ul>
+            </ListStyle>
             <button onClick={this.addWorkExperience}
             id = 'workExperience'
             style={{height: 20}}>
               Add Previous Work Experience
             </button>
           </ContainerStyle>
-          <WorkExperience>  
-            <input type="text" placeholder="Previous Job Title" id = 'salary' style = {{height: 20}}/> 
-          </WorkExperience>
+          
+           
+          
           <button type = 'submit' id = 'submit'style = {{height: 30}}>Submit</button>  
           </form>
         </FormStyle>
